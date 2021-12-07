309bd23f389783fff9c54533a1f986f4bd7c6901
Geovani Brito <geovani.br@gmail.com>
2021-12-07 02:20:54 -0300

    feat: adiciona fetch para login

--- src/component/ModalLogin/ModalLogin.js
+++ src/component/ModalLogin/ModalLogin.js
@@ -1,76 +1,124 @@
 import React, { useState } from "react";
-import { Modal, Form, Row, Col, Nav, Button } from 'react-bootstrap';
+import { Modal, Form, Row, Col, Nav, Button } from "react-bootstrap";
 import "./ModalLogin.css";
-import logo2 from '../../assets/pizza.png';
+import logo2 from "../../assets/pizza.png";
+import Cookies from "universal-cookie";
+import Homescreen from "../../Screens/Homescreen/Homescreen";
+import ReactDOM from "react-dom";
 
 function ModalLogin(props) {
-    const [email, setEmail] = useState(null);
-    const [senha, setSenha] = useState(null);
-    
-    const enviarForm = () => {
-        alert(`Email:${email}\nSenha:${senha}`);
-        props.onHide();
-    }
+  const [email, setEmail] = useState(null);
+  const [senha, setSenha] = useState(null);
 
-    return (
-        <div>
-            <Modal {...props} aria-labelledby="contained-modal-title-vcenter" dialogClassName="modal-login">
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        Faça seu Login
-                    </Modal.Title>
-                    <img className="logo-modal"
-                        alt=""
-                        src={logo2}
-                    />
-                </Modal.Header>
-                <Modal.Body className="show-grid">
-                    <Form  className="form-login">
+  const enviarForm = () => {
+    const corpo = {
+      email: email,
+      senha: senha,
+    };
 
-                        <Row className="mb-3">
-                            <Form.Group
-                                className="form-group"
-                                as={Col}
-                                controlId="formGridEmail"
-                            >
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control
-                                    form="novalidatedform"
-                                    name="email"
-                                    type="email"
-                                    placeholder="Informe seu email"
-                                    value={email}
-                                    onChange={e => setEmail(e.target.value)}
-                                />
-                            </Form.Group>
-                        </Row>
+    const cookies = new Cookies();
 
-                        <Row className="mb-3">
-                            <Form.Group
-                                className="form-group"
-                                as={Col}
-                                controlId="formGridPassword"
-                            >
-                                <Form.Label>Senha</Form.Label>
-                                <Form.Control
-                                    name="senha"
-                                    type="password"
-                                    placeholder="Informe sua senha"
-                                    value={senha}
-                                    onChange={e => setSenha(e.target.value)}
-                                />
-                            </Form.Group>
-                            <Nav.Link className="lk-home" href="/cadastro">Não é cadastrado? Clique aqui!</Nav.Link>
-                        </Row>
-                    </Form>
+    fetch("http://localhost:5000/api/v1/login", {
+      method: "POST",
+      headers: {
+        Accept: "*/*",
+      },
+      body: JSON.stringify(corpo),
+    })
+      .then(function (response) {
+        // console.log('Response: ', response);
+        if (response.status === 401) {
+          console.log("Status: ", response.statusText);
+        }
+        if (response.status === 404) {
+          console.log("Status: ", response.statusText);
+        }
+        if (response.status === 200) {
+          console.log("Status: ", response.statusText);
+        }
+        return response.json();
+      })
+      .then(function (data) {
+        if (data.status_code === 200) {
+          console.log("Logado: ", data);
+          cookies.set("token", data.token, { path: "/" });
+          console.log(cookies.get("token"));
+          console.log("Documento: ", document.cookie);
+          ReactDOM.render(
+            <React.StrictMode>
+              <Homescreen />
+            </React.StrictMode>,
+            document.getElementsByClassName("root")[0]
+          );
+        }
+        //Para deslogar:
+        //cookies.remove('token', { path: '/' });
+      });
+    props.onHide();
+  };
 
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="success" className="btn-entrar" onClick={enviarForm}>Entrar</Button>
-                </Modal.Footer>
-            </Modal>
-        </div>
-    )
+  return (
+    <div>
+      <Modal
+        {...props}
+        aria-labelledby="contained-modal-title-vcenter"
+        dialogClassName="modal-login"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Faça seu Login
+          </Modal.Title>
+          <img className="logo-modal" alt="" src={logo2} />
+        </Modal.Header>
+        <Modal.Body className="show-grid">
+          <Form className="form-login">
+            <Row className="mb-3">
+              <Form.Group
+                className="form-group"
+                as={Col}
+                controlId="formGridEmail"
+              >
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  form="novalidatedform"
+                  name="email"
+                  type="email"
+                  placeholder="Informe seu email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+            </Row>
+
+            <Row className="mb-3">
+              <Form.Group
+                className="form-group"
+                as={Col}
+                controlId="formGridPassword"
+              >
+                <Form.Label>Senha</Form.Label>
+                <Form.Control
+                  name="senha"
+                  type="password"
+                  placeholder="Informe sua senha"
+                  value={senha}
+                  onChange={(e) => setSenha(e.target.value)}
+                />
+              </Form.Group>
+              <Nav.Link className="lk-home" href="/cadastro">
+                Não é cadastrado? Clique aqui!
+              </Nav.Link>
+            </Row>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="success" className="btn-entrar" onClick={enviarForm}>
+            Entrar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 }
 
 export default ModalLogin;
