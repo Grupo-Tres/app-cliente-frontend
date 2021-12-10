872209f18518f9b3c406bd9f2c7e8503d0155770
Geovani Brito <geovani.br@gmail.com>
2021-12-10 04:24:51 -0300

    Criado fetch e intervalo de atualização

--- src/Screens/Pedidos/Pedidos.js
+++ src/Screens/Pedidos/Pedidos.js
@@ -1,23 +1,89 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './Pedidos.css';
 import Table from 'react-bootstrap/Table'
 import { Steps } from 'antd';
 import 'antd/dist/antd.css';
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
 
 function Pedidos() {
     // usar setStatus(x) com x variando de 0 a 4 para mudar a posicao da timeline
     // mapear 0 a 4 de acordo com o valor recebido do fetch do status
-    const [status, setStatus] = useState(0)
+    const [pedido, setPedido] = useState(0)
+    const [seconds, setSeconds] = useState(0)
     const { Step } = Steps;
 
-    let numPedido = 436; // obter do fetch
+    //let numPedido = 436; // obter do fetch
+
+    const cookieCarrinho = cookies.get("carrinho");
+    const idPedido = cookieCarrinho.pedido.id;
+
+
+    function atualizaPedido(dados) {
+        if (dados.status !== pedido.status){
+            setPedido(dados);
+           
+        }
+    }
+
+    //var chamada = setInterval(buscaEstado, 5000);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          //console.log(`seconds: ${seconds}`)
+            buscaEstado()
+          setSeconds(seconds + 1)
+          console.log("Estado: ", pedido.status)
+          if (pedido.status === '5'){
+            clearInterval(interval);
+            console.log("Finalizado")
+            }
+         }, 5000)
+         
+        return () => clearInterval(interval)
+       }, )
+
+    function buscaEstado() {
+        fetch("http://localhost:5000/api/v1/pedido/" + idPedido)
+        .then(function (resposta) {
+          const resultado = {
+            estado: resposta.status,
+            res: resposta.json(),
+          };
+          return resultado;
+        })
+        .then(function (respostaFinal) {
+          if (respostaFinal.estado !== 200) {
+            respostaFinal.res.then(function (erroResult) {
+              console.log(erroResult);
+            });
+          } else {
+            respostaFinal.res.then(function (dados) {
+            
+                const acompanhamento = {
+                    numeroPedido: dados.numeroPedido,
+                    status: dados.status,
+                }
+
+                atualizaPedido(acompanhamento);
+            });
+          }
+        })
+        .catch((error) => {
+          //clearInterval(chamada);
+          alert(`${error} \n Servidor backend não encontrado`);
+        });
+    }
+
+    
 
     return (
         <div className="tabela-pedidos">
             <h4>Acompanhe seus pedidos abaixo:</h4>
-            <h5>Pedido número: {numPedido}</h5>
+            <h5>Pedido número: {pedido.numeroPedido}</h5>
             <div className="steps-container">
-                <Steps direction="vertical" current={status}>
+                <Steps direction="vertical" current={pedido.status}>
                     <Step title="Realizado" description="Seu pedido foi enviado. Aguarde, por favor." />
                     <Step title="Aceito" description="Seu pedido foi aceito." />
                     <Step title="Preparando" description="Seu pedido está sendo preparado." />
