e5e4e4e174030092d3c2eeac041c887c703948de
tiagojofran <tiagojofran@hotmail.com>
2021-12-10 00:39:58 -0300

    Modificacoes no conteudo da pagina Pedidos

--- src/Screens/Pedidos/Pedidos.js
+++ src/Screens/Pedidos/Pedidos.js
@@ -1,61 +1,10 @@
 import React, { useEffect, useState } from "react";
 import './Pedidos.css';
-import Cookies from 'universal-cookie';
 import Table from 'react-bootstrap/Table'
 
 function Pedidos() {
-    // const [linhas, setLinhas] = useState(null);
-    const user = 'João';
-
-    // useEffect(() => {
-    //     if (linhas === null) {
-    //       fetch("http://localhost:5000/api/v1/pedido")
-    //         .then(function (resposta) {
-    //           const resultado = {
-    //             estado: resposta.status,
-    //             res: resposta.json(),
-    //           };
-    //           return resultado;
-    //         })
-    //         .then(function (respostaFinal) {
-    //           if (respostaFinal.estado !== 200) {
-    //             respostaFinal.res.then(function (erroResult) {
-    //               console.log(erroResult);
-    //             });
-    //           } else {
-    //             respostaFinal.res.then(function (dados) {
-    //               gerar_pedidos(dados);
-    //             });
-    //           }
-    //         })
-    //         .catch((error) => {
-    //           alert(`${error} \n Servidor backend não encontrado`);
-    //         });
-    //     }
-    //   });
-
-    // const gerar_pedidos = (dadosArray) => {
-    //     setLinhas(dadosArray.map((pedido) => {
-    //         let linha = [];
-    //         pedido.forEach((element) => {
-    //             linha.push(
-    //                 <tr>
-    //                     <td>{element.numeroPedido}</td>
-    //                     <td>{element.carrinho}</td>
-    //                     <td>{element.total}</td>
-    //                     <td>{element.comentarios}</td>
-    //                     <td>{element.createdAt}</td>
-    //                     <td>{element.status}</td>
-    //                 </tr>
-    //             );
-    //         })
-    //         return linha;
-    //     }));
-    // };
-       
     return (
-        <div className="container">
-            <h5>Olá, {user}!</h5>
+        <div className="tabela-pedidos">
             <p>Acompanhe seus pedidos abaixo:</p>
             <Table striped bordered hover size="sm">
                 <thead>
