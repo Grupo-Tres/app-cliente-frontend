750128448f4a7d2cdf9305d6463af75c61797d1b
tiagojofran <tiagojofran@hotmail.com>
2021-12-09 21:11:04 -0300

    adicao do componente Table do react-bootstrap

--- src/component/Carrinho/Carrinho.js
+++ src/component/Carrinho/Carrinho.js
@@ -3,6 +3,7 @@ import Cookies from "universal-cookie";
 import { Button } from 'react-bootstrap';
 //import ReactDOM from "react-dom";
 import React, { useState } from "react";
+import Table from 'react-bootstrap/Table'
 
 const cookies = new Cookies();
 
@@ -44,16 +45,20 @@ function Carrinho(dados) {
   return (
     <div class="carrinho">
       <div>Número do pedido: {cookieCarrinho.pedido.numeroPedido}</div>
-      <table>
-        <tr>
-          <th>Quant</th>
-          <th>Nome</th>
-          <th>Opção</th>
-          <th>Preço</th>
-          <th>Total</th>
-        </tr>
-        <React.Fragment>{linha}</React.Fragment>
-      </table>
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>Quant</th>
+            <th>Nome</th>
+            <th>Opção</th>
+            <th>Preço</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <React.Fragment>{linha}</React.Fragment>
+        </tbody>
+      </Table>
 
       <div>Subtotal: {subTotal}</div>
       <div>Taxa de entrega: {taxaEntrega}</div>
